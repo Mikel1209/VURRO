79fd6b3f11881299f2f7f49b76ede0ffccd9d09a
Mike <mikeangellopez1@gmail.com>
2019-04-18 13:14:52 -0500

    MANDE

--- app/src/main/java/com/example/mikea/vurro/INICIO.java
+++ app/src/main/java/com/example/mikea/vurro/INICIO.java
@@ -8,6 +8,6 @@ public class INICIO extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicio);//WERGAs
+        setContentView(R.layout.activity_inicio);
     }
 }
