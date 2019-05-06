083e5af5de930b565766b1cd4d0796d563c8ab1d
Mike <mikeangellopez1@gmail.com>
2019-05-05 21:30:57 -0500

    ACTUALIZACION -------------------- *Error de Sesion de inicio vacio solucionado #HailHydra!

--- app/src/main/java/com/example/mikea/vurro/MENU.java
+++ app/src/main/java/com/example/mikea/vurro/MENU.java
@@ -24,58 +24,47 @@ public class MENU extends AppCompatActivity {
         /*
             @ Aquí se manda a los activitys correspondientes
          */
-        View.OnClickListener Conectar = new View.OnClickListener() {
+
+        btnConectar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i= new Intent(getApplicationContext(),Registro.class);
+            public void onClick(View v)
+            {
+                Intent i= new Intent(getApplicationContext(),Conexion.class);
                 startActivity(i);
             }
-        };
-        btnConectar.setOnClickListener(Conectar);
-        View.OnClickListener Manejo = new View.OnClickListener() {
+        });
+        btnManejo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 Intent i= new Intent(getApplicationContext(),Manejar.class);
                 startActivity(i);
             }
-        };
-        btnManejo.setOnClickListener(Manejo);
-
-        View.OnClickListener Usuarios = new View.OnClickListener() {
+        });
+        btnCajas.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i= new Intent(getApplicationContext(),Registro.class);
-                startActivity(i);
-            }
-        };
-        btnUsuarios.setOnClickListener(Usuarios);
-        View.OnClickListener Cajas = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 Intent i= new Intent(getApplicationContext(),Cajas.class);
                 startActivity(i);
             }
-        };
-        btnCajas.setOnClickListener(Cajas);
-
-        View.OnClickListener Rutas = new View.OnClickListener() {
+        });
+        btnRutas.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 Intent i= new Intent(getApplicationContext(),Rutas.class);
                 startActivity(i);
             }
-        };
-        btnRutas.setOnClickListener(Rutas);
-
-        View.OnClickListener Manual = new View.OnClickListener() {
+        });
+        btnManual.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 Intent i= new Intent(getApplicationContext(),Manual.class);
                 startActivity(i);
             }
-        };
-        btnManual.setOnClickListener(Manual);
+        });
 
         /*
             @Aquí acaba de mandar a los activitys correspondientes
